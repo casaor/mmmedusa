40e6c19edc6fcde0dcd1621dca8aaa52de0ec0a0
carlota sas <casaor@outlook.com>
2020-12-21 13:45:11 +0100

    nav i portada

---
+++ js/slider.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.slider').bxSlider({
+    mode: 'horizontal',
+    infiniteLoop: true,
+    speed: 1500,
+    pause: 7000,
+    auto: true,
+    pager: false,
+    controls: false,
+    autoHover:true
+    }); 
+});
